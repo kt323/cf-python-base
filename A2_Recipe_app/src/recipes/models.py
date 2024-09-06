a249c0121734f0b412bdc00f281c4cc4b8fe6403
Katie Ho <kthodesign@gmail.com>
2024-09-05 23:05:49 -0700

    2.3

--- A2_Recipe_app/src/recipes/models.py
+++ A2_Recipe_app/src/recipes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipes(models.Model):
+    name = models.CharField(max_length=120)
+    cooking_time = models.FloatField(help_text="In minutes", default=0, null=True)
+    ingredients = models.TextField(default='')
+    description = models.TextField(default='')
+
+    def __str__(self):
+        return str(self.name)
