90e092b432d71675bd5ba6dd3a1e8b3e46e42c88
Katie Ho <kthodesign@gmail.com>
2024-09-08 17:29:26 -0700

    Delete A2_Recipe_app/src/recipes directory

--- A2_Recipe_app/src/recipes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Recipes
-
-# Register your models here.
-
-admin.site.register(Recipes)
-class RecipesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'cooking_time')
-    search_fields = ('name', 'ingredients')
