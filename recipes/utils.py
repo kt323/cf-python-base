6b46891d313c1b06975c67a3af4af09f985f5ee7
Katie Ho <kthodesign@gmail.com>
2024-09-10 22:30:47 -0700

    Update utils.py

--- recipes/utils.py
+++ recipes/utils.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 import base64
 import matplotlib.pyplot as plt
+import numpy as np
 
 def get_graph():
     buffer = BytesIO()
@@ -15,19 +16,32 @@ def get_graph():
 def get_chart(chart_type, data, **kwargs):
     plt.switch_backend('AGG')
     fig = plt.figure(figsize=(6, 3))
-
-    if chart_type == 'bar-chart':
-        plt.bar(data['name'], data['cooking_time'])
-
-    elif chart_type == 'pie-chart':
-        labels = kwargs.get('labels')
-        plt.pie(data['cooking_time'], labels=labels)
-
-    elif chart_type == 'line-chart':
-        plt.plot(data['name'], data['cooking_time'])
+    
+    if 'Name' in data.columns and 'Cooking Time' in data.columns:
+        if chart_type == 'bar-chart':
+            plt.bar(data['Name'], data['Cooking Time'])
+        elif chart_type == 'pie-chart':
+            plt.pie(data['Cooking Time'], labels=data['Name'], autopct='%1.1f%%')
+        elif chart_type == 'line-chart':
+            plt.plot(data['Name'], data['Cooking Time'])
+        else:
+            print('unknown chart type')
+        
+        plt.title(f'{chart_type.replace("-", " ").title()} of Cooking Times')
+        plt.xlabel('Recipe Name')
+        plt.ylabel('Cooking Time (minutes)')
     else:
-        print('unknown chart type')
-
+        plt.text(0.5, 0.5, 'No data available', ha='center', va='center')
+    
     plt.tight_layout()
     chart = get_graph()
     return chart
+
+def rename_columns(df):
+    column_mapping = {
+        'name': 'Name',
+        'cooking_time': 'Cooking Time',
+        'ingredients': 'Ingredients',
+        'difficulty': 'Difficulty'
+    }
+    return df.rename(columns=column_mapping)
